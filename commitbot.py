bc6b0642860a77c3d47ad432b60dfaeef04164b0
Junkyu Park <junk3843@naver.com>
2017-03-13 21:38:45 +0900

    Adjust INI Config file

--- commitbot.py
+++ commitbot.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from slacker import Slacker
+import configparser
 
 
-from configs import TOKEN
-from configs import COMMIT_CHANNEL
 from github import get_all_repos
 from github import check_user_commit
 
+
+Config = configparser.ConfigParser()
+Config.read('configs.ini')
+
+TOKEN = Config.get('Configuration', 'TOKEN')
+COMMIT_CHANNEL = Config.get('Configuration', 'COMMIT_CHANNEL')
 
 slack = Slacker(TOKEN)
 
@@ -17,7 +22,7 @@ def send_commit_warning(username):
     attachments_dict['title'] = "커밋하라"
     attachments_dict['title_link'] = "Do Commit!"
     attachments_dict['fallback'] = "오늘 하루가 얼마 남지 않았어!"
-    attachments_dict['text'] = "%s님 커밋하세요!" % (username)
+    attachments_dict['text'] = "%s님 커밋하세요![(프로토타입은 master branch만 체크합니다.)]" % (username)
     attachments_dict['mrkdwn_in'] = ['text', 'pretext']
     attachments = [attachments_dict]
 
@@ -29,11 +34,5 @@ def send_commit_warning_to_member(username):
     committed = check_user_commit(username, repos)
 
     if committed is False:
-        print("Send Slack Warning!")
+        print("[%s]Send Slack Warning!" % username)
         send_commit_warning(username)
-
-
-usernames = ['eminentstar']
-
-for username in usernames:
-    send_commit_warning_to_member(username)
